90ad971d8cf000c5d3f61c64840b746237da4460
shakilkhan95 <th.shakilkhan@gmail.com>
2026-02-15 12:35:41 +0400

    new learning delegation and propagation applied

--- script.js
+++ script.js
@@ -4,26 +4,19 @@ const greenBtn = document.getElementById('green');
 const yellowBtn = document.getElementById('yellow');
 const blueBtn = document.getElementById('blue');
 const randomBtn = document.getElementById('random');
+const btnGroup = document.getElementById('btnGroup');
 
-redBtn.addEventListener('click', () => {
-    colorBox.style.backgroundColor = 'red';
-});
+//Delegation event handler
+btnGroup.addEventListener('click', function(event){
+    const colorName = event.target.innerText.toLowerCase();
 
-greenBtn.addEventListener('click', () => {
-    colorBox.style.backgroundColor = 'green';
-});
+    if(colorName === 'random'){
+        //Random color generator
+        const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        colorBox.style.backgroundColor = randomColor;
+        
+    } else {
+        colorBox.style.backgroundColor = colorName;
+    }
+})
 
-yellowBtn.addEventListener('click', () => {
-    colorBox.style.backgroundColor = 'yellow';
-});
-
-blueBtn.addEventListener('click', () => {
-    colorBox.style.backgroundColor = 'blue';
-});
-
-randomBtn.addEventListener('click', () => {
-    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    colorBox.style.backgroundColor = randomColor;
-});
-
-console.log(hello;)
