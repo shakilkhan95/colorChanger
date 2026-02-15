350334a9323957f9b9ac9f8c24366403f486c784
shakilkhan95 <th.shakilkhan@gmail.com>
2026-02-15 12:16:52 +0400

    testing git remote

--- script.js
+++ script.js
@@ -25,3 +25,5 @@ randomBtn.addEventListener('click', () => {
     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
     colorBox.style.backgroundColor = randomColor;
 });
+
+console.log(hello;)
