4a175f059fb398425912d43dd314d3b7c265eab1
shakilkhan95 <th.shakilkhan@gmail.com>
2026-02-15 13:48:56 +0400

    Issue of target while clicking empty space of parent div fixed

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const colorBox = document.getElementById('colorBox');
-const randomBtn = document.getElementById('random');
 const btnGroup = document.getElementById('btnGroup');
 
 //Delegation event handler
 btnGroup.addEventListener('click', function(event){
+    //checking if empty space of parent div clicked
+    if(!event.target.matches('button')) return;
+
     const colorName = event.target.innerText.toLowerCase();
 
     if(colorName === 'random'){
         //Random color generator
         const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
         colorBox.style.backgroundColor = randomColor;
-        randomBtn.style.backgroundColor = randomColor;
+        event.target.style.backgroundColor = randomColor;
         
     } else {
         colorBox.style.backgroundColor = colorName;
